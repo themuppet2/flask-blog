7ab8e4bc9231f5bf15088f30d72ab9105edfe908
James Collins <j_collins@mac.com>
2016-04-27 18:17:21 +0100

    Now have a working 'site' which allows user to login, upon which they are directed to /main

--- blog.py
+++ blog.py
@@ -3,10 +3,15 @@
 # imports
 from flask import Flask, render_template, request, session, \
 flash, redirect, url_for, g
+from functools import wrap
 import sqlite3
 
 # configuration
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'hard_to_guess' # this needs to be impossible to
+# guess!! Use random key generator, eg import os, os.urandom(24).
 
 app = Flask(__name__)
 
@@ -17,13 +22,28 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    #flash(request.method)
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or\
+        request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+    return render_template('login.html', error=error)
 
 @app.route('/main')
 def main():
     return render_template('main.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
