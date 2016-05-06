e5a8d91ceb2e7f272188c06288cc7d90bc780a79
James Collins <j_collins@mac.com>
2016-05-06 18:11:40 +0100

    Added posts to /main

--- blog.py
+++ blog.py
@@ -48,7 +48,12 @@ def login():
 @app.route('/main')
 @login_required
 def main():
-    return render_template('main.html')
+    g.db = connect_db()
+    cur = g.db.execute('SELECT * FROM posts')
+    posts = [dict(title=row[0], post=row[1])
+    for row in cur.fetchall()]
+    g.db.close()
+    return render_template('main.html', posts=posts)
 
 @app.route('/logout')
 def logout():
